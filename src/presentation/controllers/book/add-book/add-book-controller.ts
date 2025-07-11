316ed9649d8b4d7758dba5609c5cd4783012b8ff
Hernany Motoso <hernanymotoso@gmail.com>
2025-07-11 17:00:42 -0300

    feat: ensure AddBookController  call addBook with correct values

--- src/presentation/controllers/book/add-book/add-book-controller.ts
+++ src/presentation/controllers/book/add-book/add-book-controller.ts
@@ -1,8 +1,11 @@
+import { AddBook } from '../../../../domain/usecases/book/add-book'
 import { MissingParamError } from '../../../errors'
 import { badRequest } from '../../../helpers/http-helpers'
 import { Controller, HttpRequest, HttpResponse } from '../../../protocols'
 
 export class AddBookController implements Controller {
+  constructor (private readonly addBook: AddBook) {}
+
   async handle (httpRequest: HttpRequest): Promise<HttpResponse> {
     if (!httpRequest.body.title) {
       return badRequest(new MissingParamError('title'))
@@ -10,5 +13,6 @@ export class AddBookController implements Controller {
     if (!httpRequest.body.author) {
       return badRequest(new MissingParamError('author'))
     }
+    await this.addBook.add(httpRequest.body)
   }
 }
