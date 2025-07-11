806fc3e8f9a4d1328ebedfd959fdc6da3f274248
Hernany Motoso <hernanymotoso@gmail.com>
2025-07-11 17:04:15 -0300

    feat: ensure AddBookController  return 500 if AddBook throws

--- src/presentation/controllers/book/add-book/add-book-controller.ts
+++ src/presentation/controllers/book/add-book/add-book-controller.ts
@@ -1,18 +1,22 @@
 import { AddBook } from '../../../../domain/usecases/book/add-book'
 import { MissingParamError } from '../../../errors'
-import { badRequest } from '../../../helpers/http-helpers'
+import { badRequest, serverError } from '../../../helpers/http-helpers'
 import { Controller, HttpRequest, HttpResponse } from '../../../protocols'
 
 export class AddBookController implements Controller {
   constructor (private readonly addBook: AddBook) {}
 
   async handle (httpRequest: HttpRequest): Promise<HttpResponse> {
-    if (!httpRequest.body.title) {
-      return badRequest(new MissingParamError('title'))
+    try {
+      if (!httpRequest.body.title) {
+        return badRequest(new MissingParamError('title'))
+      }
+      if (!httpRequest.body.author) {
+        return badRequest(new MissingParamError('author'))
+      }
+      await this.addBook.add(httpRequest.body)
+    } catch (error) {
+      return serverError()
     }
-    if (!httpRequest.body.author) {
-      return badRequest(new MissingParamError('author'))
-    }
-    await this.addBook.add(httpRequest.body)
   }
 }
