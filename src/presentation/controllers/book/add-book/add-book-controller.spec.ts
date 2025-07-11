b03e61bee3571629b82ec51555eb3f5c15971c2b
Hernany Motoso <hernanymotoso@gmail.com>
2025-07-11 16:55:37 -0300

    test: ensure AddBookController return 400 if no title is provided

---
+++ src/presentation/controllers/book/add-book/add-book-controller.spec.ts
@@ -0,0 +1,37 @@
+import MockDate from 'mockdate'
+import { AddBookController } from './add-book-controller'
+import { badRequest } from '../../../helpers/http-helpers'
+import { MissingParamError } from '../../../errors'
+
+type SutTypes = {
+  sut: AddBookController
+}
+
+const makeSut = (): SutTypes => {
+  const sut = new AddBookController()
+
+  return {
+    sut
+  }
+}
+
+describe('AddBook Controller', () => {
+  beforeAll(() => {
+    MockDate.set(new Date())
+  })
+
+  afterAll(() => {
+    MockDate.reset()
+  })
+
+  it('should return 400 if no title is provided', async () => {
+    const { sut } = makeSut()
+    const httpRequest = {
+      body: {
+        author: 'any_author'
+      }
+    }
+    const httpResponse = await sut.handle(httpRequest)
+    expect(httpResponse).toEqual(badRequest(new MissingParamError('title')))
+  })
+})
