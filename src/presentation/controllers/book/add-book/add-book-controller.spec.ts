e26bb35ae5baa47cf219eb5298d677a55cc2a6ed
Hernany Motoso <hernanymotoso@gmail.com>
2025-07-11 16:57:26 -0300

    test: ensure AddBookController return 400 if no author is provided

--- src/presentation/controllers/book/add-book/add-book-controller.spec.ts
+++ src/presentation/controllers/book/add-book/add-book-controller.spec.ts
@@ -34,4 +34,15 @@ describe('AddBook Controller', () => {
     const httpResponse = await sut.handle(httpRequest)
     expect(httpResponse).toEqual(badRequest(new MissingParamError('title')))
   })
+
+  it('should return 400 if no author is provided', async () => {
+    const { sut } = makeSut()
+    const httpRequest = {
+      body: {
+        title: 'any_title'
+      }
+    }
+    const httpResponse = await sut.handle(httpRequest)
+    expect(httpResponse).toEqual(badRequest(new MissingParamError('author')))
+  })
 })
