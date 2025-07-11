1dab793dba673192912933e0bcfe9e065ec3beb1
Hernany Motoso <hernanymotoso@gmail.com>
2025-07-11 17:39:23 -0300

    feat: ensure BookMockRepository load all books on success and return books

--- src/infra/db/mockdb/book/book-mock-repository.ts
+++ src/infra/db/mockdb/book/book-mock-repository.ts
@@ -1,9 +1,10 @@
 import { AddBookRepository } from '../../../../data/protocols/db/book/add-book-repository'
+import { LoadBooksRepository } from '../../../../data/protocols/db/book/load-books-repository'
 import { BookModel } from '../../../../domain/models/book'
 import { AddBookParams } from '../../../../domain/usecases/book/add-book'
 import { mockDb } from '../helpers/mock-db-helper'
 
-export class BookMockRepository implements AddBookRepository {
+export class BookMockRepository implements AddBookRepository, LoadBooksRepository {
   async add (data: AddBookParams): Promise<BookModel> {
     const randomId = (): string => Math.random().toString(36).substring(2, 10)
     const book: BookModel = {
@@ -13,5 +14,9 @@ export class BookMockRepository implements AddBookRepository {
     }
     mockDb.books.push(book)
     return book
+  }
+
+  async loadAll (): Promise<BookModel[]> {
+    return mockDb.books
   }
 }
