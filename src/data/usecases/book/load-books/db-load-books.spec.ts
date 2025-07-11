1c793c58df40d50cc795fd79af2520f7fab2ebbb
Hernany Motoso <hernanymotoso@gmail.com>
2025-07-11 17:32:08 -0300

    test: ensure DbLoadBooks call LoadBooksRepository

---
+++ src/data/usecases/book/load-books/db-load-books.spec.ts
@@ -0,0 +1,59 @@
+import MockDate from 'mockdate'
+import { DbLoadBooks } from './db-load-books'
+import { BookModel, LoadBooksRepository } from './db-load-books-protocols'
+
+const makeBookModels = (): BookModel[] => {
+  return [{
+    id: 'any_id',
+    title: 'any_title',
+    author: 'any_author',
+    createdAt: new Date().getTime()
+  },
+  {
+    id: 'other_id',
+    title: 'other_title',
+    author: 'other_author',
+    createdAt: new Date().getTime()
+  }]
+}
+
+const makeLoadBooksRepository = (): LoadBooksRepository => {
+  class LoadBooksRepositoryStub implements LoadBooksRepository {
+    async loadAll (): Promise<BookModel[]> {
+      return await Promise.resolve(makeBookModels())
+    }
+  }
+  return new LoadBooksRepositoryStub()
+}
+
+type SutTypes = {
+  sut: DbLoadBooks
+  loadBooksRepositoryStub: LoadBooksRepository
+}
+
+const makeSut = (): SutTypes => {
+  const loadBooksRepositoryStub = makeLoadBooksRepository()
+  const sut = new DbLoadBooks(loadBooksRepositoryStub)
+
+  return {
+    sut,
+    loadBooksRepositoryStub
+  }
+}
+
+describe('DbLoadBooks UseCase', () => {
+  beforeAll(() => {
+    MockDate.set(new Date())
+  })
+
+  afterAll(() => {
+    MockDate.reset()
+  })
+
+  it('should call LoadBooksRepository', async () => {
+    const { sut, loadBooksRepositoryStub } = makeSut()
+    const loadAllSpy = jest.spyOn(loadBooksRepositoryStub, 'loadAll')
+    await sut.load()
+    expect(loadAllSpy).toHaveBeenCalled()
+  })
+})
