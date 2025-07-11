176b39b244b0363eb572bcf393a9956c01bd4b95
Hernany Motoso <hernanymotoso@gmail.com>
2025-07-11 17:43:51 -0300

    test: ensure LoadBooksController call LoadBooks

---
+++ src/presentation/controllers/book/load-books/load-books-controller.spec.ts
@@ -0,0 +1,62 @@
+import { BookModel } from '../../../../domain/models/book'
+import { LoadBooks } from '../../../../domain/usecases/book/load-books'
+import MockDate from 'mockdate'
+import { LoadBooksController } from './load-books-controller'
+
+const makeBookModels = (): BookModel[] => {
+  return [{
+    id: 'any_id',
+    title: 'any_title',
+    author: 'any_author',
+    createdAt: new Date().getTime()
+  },
+  {
+    id: 'other_id',
+    title: 'other_title',
+    author: 'other_author',
+    createdAt: new Date().getTime()
+  }]
+}
+
+const makeLoadBooks = (): LoadBooks => {
+  class LoadBooksStub implements LoadBooks {
+    load = async (): Promise<BookModel[]> => {
+      return await new Promise(resolve => {
+        resolve(makeBookModels())
+      })
+    }
+  }
+  return new LoadBooksStub()
+}
+
+type SutTypes = {
+  sut: LoadBooksController
+  loadBooksStub: LoadBooks
+}
+
+const makeSut = (): SutTypes => {
+  const loadBooksStub = makeLoadBooks()
+  const sut = new LoadBooksController(loadBooksStub)
+
+  return {
+    sut,
+    loadBooksStub
+  }
+}
+
+describe('LoadBooks Controller', () => {
+  beforeAll(() => {
+    MockDate.set(new Date())
+  })
+
+  afterAll(() => {
+    MockDate.reset()
+  })
+
+  it('should call LoadBooks', async () => {
+    const { sut, loadBooksStub } = makeSut()
+    const loadSty = jest.spyOn(loadBooksStub, 'load')
+    await sut.handle({})
+    expect(loadSty).toHaveBeenCalled()
+  })
+})
