a4c97c4cef154cf9b00cbf428d6e24977688bcad
Hernany Motoso <hernanymotoso@gmail.com>
2025-07-11 17:45:12 -0300

    feat: ensure LoadBooksController return 200 on success

--- src/presentation/controllers/book/load-books/load-books-controller.ts
+++ src/presentation/controllers/book/load-books/load-books-controller.ts
@@ -1,11 +1,12 @@
 import { LoadBooks } from '../../../../domain/usecases/book/load-books'
+import { ok } from '../../../helpers/http-helpers'
 import { Controller, HttpRequest, HttpResponse } from '../../../protocols'
 
 export class LoadBooksController implements Controller {
   constructor (private readonly loadBooks: LoadBooks) {}
 
   async handle (httpRequest: HttpRequest): Promise<HttpResponse> {
-    await this.loadBooks.load()
-    return null
+    const books = await this.loadBooks.load()
+    return ok(books)
   }
 }
