0cee5d5b821b65fb4aad5e97c5753663b76d02eb
Hernany Motoso <hernanymotoso@gmail.com>
2025-07-11 16:45:22 -0300

    test: ensure BookMockRepository  add a book on success

---
+++ src/infra/db/mockdb/book/book-mock-repository.spec.ts
@@ -0,0 +1,32 @@
+import { mockDb } from '../helpers/mock-db-helper'
+import { BookMockRepository } from './book-mock-repository'
+
+const makeSut = (): BookMockRepository => {
+  return new BookMockRepository()
+}
+
+describe('Book Mock Repository ', () => {
+  describe('add()', () => {
+    beforeAll(() => {
+      mockDb.books = []
+    })
+
+    afterAll(() => {
+      mockDb.books = []
+    })
+
+    it('should add a book on success', async () => {
+      const sut = makeSut()
+
+      await sut.add({
+        title: 'any_title',
+        author: 'any_author'
+      })
+
+      expect(mockDb.books.length).toBe(1)
+      expect(mockDb.books[0].id).toBeTruthy()
+      expect(mockDb.books[0].title).toBe('any_title')
+      expect(mockDb.books[0].author).toBe('any_author')
+    })
+  })
+})
