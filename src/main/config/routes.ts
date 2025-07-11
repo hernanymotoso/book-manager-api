d8ad6d760e727c42c5cef2e79c50d1c09b5444f4
Hernany Motoso <hernanymotoso@gmail.com>
2025-07-11 17:25:49 -0300

    feat: ensure book router return 200 on add book

---
+++ src/main/config/routes.ts
@@ -0,0 +1,17 @@
+import { Express, Router } from 'express'
+import { readdirSync } from 'fs'
+import path from 'path'
+
+export default (app: Express): void => {
+  const router = Router()
+  app.use('/api', router)
+
+  const routesPath = path.join(__dirname, '..', 'routes')
+
+  readdirSync(routesPath).map(async file => {
+    if (!file.includes('.test') && !file.endsWith('.map')) {
+      const route = await import(path.join(routesPath, file))
+      route.default(router)
+    }
+  })
+}
