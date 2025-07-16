e142f9180ac8a5d21bc1c8f940e14c2866039101
Hernany Motoso <hernanymotoso@gmail.com>
2025-07-16 09:29:26 -0300

    feat: ensure LoadBooksController return 500 if LoadBooks throws

--- src/presentation/controllers/book/load-books/load-books-controller.ts
+++ src/presentation/controllers/book/load-books/load-books-controller.ts
@@ -1,12 +1,16 @@
 import { LoadBooks } from '../../../../domain/usecases/book/load-books'
-import { noContent, ok } from '../../../helpers/http-helpers'
+import { noContent, ok, serverError } from '../../../helpers/http-helpers'
 import { Controller, HttpRequest, HttpResponse } from '../../../protocols'
 
 export class LoadBooksController implements Controller {
   constructor (private readonly loadBooks: LoadBooks) {}
 
   async handle (httpRequest: HttpRequest): Promise<HttpResponse> {
-    const books = await this.loadBooks.load()
-    return books.length ? ok(books) : noContent()
+    try {
+      const books = await this.loadBooks.load()
+      return books.length ? ok(books) : noContent()
+    } catch (error) {
+      return serverError()
+    }
   }
 }
