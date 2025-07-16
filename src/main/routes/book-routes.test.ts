ca578a6b36fe81cc1917153a412b3e4231334c2a
Hernany Motoso <hernanymotoso@gmail.com>
2025-07-16 10:19:50 -0300

    test: implement auth middleware on book routes and update related tests

--- src/main/routes/book-routes.test.ts
+++ src/main/routes/book-routes.test.ts
@@ -3,8 +3,10 @@ import app from '../config/app'
 
 describe('Book Routes', () => {
   describe('POST /books', () => {
+    const accessToken = 'valid_token'
     it('should return 200 on add book', async () => {
       await request(app).post('/api/books')
+        .set('x-access-token', accessToken)
         .send({
           title: 'any_title',
           author: 'any_author'
@@ -15,13 +17,15 @@ describe('Book Routes', () => {
 
   describe('Get /books', () => {
     it('should return 200 when get books', async () => {
+      const accessToken = 'valid_token'
       await request(app).post('/api/books')
+        .set('x-access-token', accessToken)
         .send({
           title: 'any_title',
           author: 'any_author'
         })
 
-      await request(app).get('/api/books').expect(200)
+      await request(app).get('/api/books').set('x-access-token', accessToken).expect(200)
     })
   })
 })
