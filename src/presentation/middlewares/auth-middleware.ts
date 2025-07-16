bdfbf2e6cb1d33fe8c7bd4196a2f512e0526f990
Hernany Motoso <hernanymotoso@gmail.com>
2025-07-16 10:13:21 -0300

    feat: ensure AuthMiddleware  return 500 if ValidateUserToken throws

--- src/presentation/middlewares/auth-middleware.ts
+++ src/presentation/middlewares/auth-middleware.ts
@@ -1,17 +1,21 @@
 import { ValidateUserToken } from '../../domain/usecases/user/validate-user-token'
 import { AccessDeniedError } from '../errors'
-import { forbidden, ok } from '../helpers/http-helpers'
+import { forbidden, ok, serverError } from '../helpers/http-helpers'
 import { HttpRequest, HttpResponse, Middleware } from '../protocols'
 
 export class AuthMiddleware implements Middleware {
   constructor (private readonly validateUserToken: ValidateUserToken) {}
 
   async handle (httpRequest: HttpRequest): Promise<HttpResponse> {
-    const accessToken = httpRequest.headers?.['x-access-token']
-    const isValid = await this.validateUserToken.validate(accessToken)
-    if (isValid) {
-      return ok({})
+    try {
+      const accessToken = httpRequest.headers?.['x-access-token']
+      const isValid = await this.validateUserToken.validate(accessToken)
+      if (isValid) {
+        return ok({})
+      }
+      return forbidden(new AccessDeniedError())
+    } catch (error) {
+      return serverError()
     }
-    return forbidden(new AccessDeniedError())
   }
 }
